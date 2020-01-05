6e6023696a694cf84ee36dd1b6fd2b6b279e3a9d
Terry Threatt <terry.threatt@gmail.com>
2020-01-05 20:38:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  if value_to_find in array do 
+  return value_to_find
+end
 end
 
 def find_max_value(array)
   # Add your solution here
+  return array.
 end
 
 def find_min_value(array)
