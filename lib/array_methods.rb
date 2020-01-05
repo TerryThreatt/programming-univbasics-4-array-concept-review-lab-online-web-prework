0689d2463b0b434b7b81b79ce063108b74c4b304
Terry Threatt <terry.threatt@gmail.com>
2020-01-05 20:49:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,10 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  return array.
+  return array.largest
 end
 
 def find_min_value(array)
   # Add your solution here
+  return array.smallest
 end
